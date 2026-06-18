bc888a30c3c1d4f96ec4291ebb422e30eacb0ef1
GuillemVaquer <vaker2@gmail.com>
2026-06-18 16:25:42 +0200

    intente que els .css també es detecten en DO

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,7 @@ import './stimulus_bootstrap.js';
  */
 import './styles/app.css';
 
+import './styles/custom.css';
+import 'bootstrap';
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
